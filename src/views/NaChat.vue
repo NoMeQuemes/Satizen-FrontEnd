8dd4ecb777ed1d74922fb81c8b857cfda81778cb
Ignacioorosco <nachoron32@gmail.com>
2024-12-03 21:10:10 -0300

    cambios en chat

--- src/views/NaChat.vue
+++ src/views/NaChat.vue
@@ -1,54 +1,54 @@
 <template>
   <div class="app-container">
-    <BarraLateral />
+    <SideBar/>
     <div class="main-content">
-      <EncabezadoPrincipal></EncabezadoPrincipal>
       <div class="chat-container">
         <ChatList />
-        <ChatView />
+        <MyComponent />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import EncabezadoPrincipal from '@/components/EncabezadoPrincipal.vue';
-import BarraLateral from '@/components/BarraLateral.vue';
-import ChatList from '@/components/ChatList.vue';
-import ChatView from '@/components/ChatView.vue';
-import EncabezadoPrincipal from '@/components/EncabezadoPrincipal.vue';
-export default {
-  components: { Encabezado, BarraLateral, ChatList, ChatView },
-  name: "PatientList",
-};
+ // import Encabezado from '@/components/Encabezado.vue';
+ // import BarraLateral from '@/components/BarraLateral.vue';
+  import ChatList from '@/components/ChatList.vue';
+  import MyComponent from '@/components/MyComponent.vue';
+  import SideBar from '@/components/SideBar.vue';
+
+  export default {
+      components: {  SideBar,ChatList, MyComponent},
+      name: "PatientList",
+  };
 </script>
 
 <style>
 .app-container {
-  display: flex;
-  height: 100vh;
-  overflow: hidden;
+display: flex;
+height: 100vh; 
+overflow: hidden; 
 }
 
 .main-content {
-  flex-grow: 2;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
+flex-grow: 1;
+display: flex;
+flex-direction: column;
+height: 100vh;
 }
 
 .chat-container {
-  display: flex;
-  flex-grow: 1;
-  overflow: hidden;
+display: flex;
+flex-grow: 1;
+overflow: hidden;
 }
 
-.main-content>.topbar {
-  flex-shrink: 0;
+.main-content > .topbar {
+flex-shrink: 0;
 }
 
-.chat-container>* {
-  flex-grow: 1;
-  overflow-y: auto;
+.chat-container > * {
+flex-grow: 1;
+overflow-y: auto;
 }
 </style>
